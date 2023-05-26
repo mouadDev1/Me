a906ac80e00ccd81d004160c0efc43d7d8e480ce
mouadDev1 <132854935+mouadDev1@users.noreply.github.com>
2023-05-26 12:08:56 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -16,31 +16,99 @@
     new WOW().init();
 
 
-    // Navbar on scrolling
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.navbar').fadeIn('slow').css('display', 'flex');
+   
+
+    
+
+    
+
+    
+    $(document).ready(function () {
+      var isScrolling = false;
+      var $navbarCollapse = $('.navbar-collapse');
+    
+      // Add or remove 'navbar-scrolled' class based on scroll position
+      function updateNavbar() {
+        var scrollDistance = $(window).scrollTop();
+        if (scrollDistance > 300) {
+          $('.navbar').addClass('navbar-scrolled');
         } else {
-            $('.navbar').fadeOut('slow').css('display', 'none');
+          $('.navbar').removeClass('navbar-scrolled');
         }
-    });
-
-
-    // Smooth scrolling on the navbar links
-    $(".navbar-nav a").on('click', function (event) {
-        if (this.hash !== "") {
-            event.preventDefault();
-            
-            $('html, body').animate({
-                scrollTop: $(this.hash).offset().top - 45
-            }, 10, 'easeInOutExpo');
-            
-            if ($(this).parents('.navbar-nav').length) {
-                $('.navbar-nav .active').removeClass('active');
-                $(this).closest('a').addClass('active');
-            }
+      }
+    
+      // Assign active class to nav links while scrolling
+      function updateActiveSection() {
+        var scrollDistance = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        var isBottomReached = (scrollDistance + windowHeight) >= $(document).height();
+    
+        $('.section').each(function () {
+          var sectionTop = $(this).offset().top;
+          var sectionHeight = $(this).outerHeight();
+    
+          if (
+            (scrollDistance >= sectionTop - windowHeight / 2 && scrollDistance < sectionTop + sectionHeight - windowHeight / 2) ||
+            (isBottomReached && $(this).is(':last-child'))
+          ) {
+            var sectionId = $(this).attr('id');
+            $('.navbar-nav a').removeClass('active');
+            $('.navbar-nav a[href="#' + sectionId + '"]').addClass('active');
+          }
+        });
+      }
+    
+      // Scroll event listener
+      $(window).scroll(function () {
+        if (!isScrolling) {
+          isScrolling = true;
+          updateNavbar();
+          updateActiveSection();
+          setTimeout(function () {
+            isScrolling = false;
+          }, 250);
         }
+      });
+    
+      // Refresh scroll navigation on page load
+      updateNavbar();
+      updateActiveSection();
+    
+      // Add click event listener to nav links
+      $('.navbar-nav a').on('click', function (event) {
+        event.preventDefault();
+        var targetSection = $(this).attr('href');
+        var targetOffset = $(targetSection).offset().top;
+        var navHeight = $('.navbar').outerHeight();
+        var scrollDistance = targetOffset;
+    
+        $('html, body').animate({
+          scrollTop: scrollDistance
+        }, 100, function () {
+          updateActiveSection();
+        });
+    
+        // Close the navbar after clicking a navigation item
+        $navbarCollapse.collapse('hide');
+    
+        $('.navbar-nav a').removeClass('active');
+        $(this).addClass('active');
+      });
+    
+      // Refresh scroll navigation on window resize
+      $(window).resize(function () {
+        updateActiveSection();
+      });
+    
+      // Refresh scroll navigation on content change
+      $(window).on('load', function () {
+        updateActiveSection();
+      });
     });
+    
+    
+      
+      
     
     
     // Back to top button
@@ -68,6 +136,31 @@
             loop: true
         });
     }
+
+
+
+    
+    $(document).ready(function() {
+  $('.refresh-link').click(function(e) {
+    e.preventDefault();
+
+    // Restore the original color and remove any selection styles from the icon and link
+    var link = $(this);
+    var icon = link.find('i');
+    
+    // Remove any classes that modify the icon's color or hover effects
+    icon.removeClass('clicked'); // Replace 'clicked' with the class name that changes the color
+    icon.removeClass('hover-class'); // Replace 'hover-class' with any class applied on hover
+    
+    link.blur(); // Remove focus from the link
+    
+    // Allow the link to open in a new tab after a short delay (e.g., 300 milliseconds)
+    setTimeout(function() {
+      var newTab = window.open(link.attr('href'), '_blank');
+      newTab.focus();
+    }, 300);
+  });
+});
 
 
    
