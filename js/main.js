a0d66de7ce2f43d5538a1caa2d040bbceee5ba16
mouadDev1 <132854935+mouadDev1@users.noreply.github.com>
2023-06-16 08:36:46 -0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,213 +1,180 @@
 (function ($) {
-    "use strict";
+  "use strict";
 
-    // Spinner
-    var spinner = function () {
-        setTimeout(function () {
-            if ($('#spinner').length > 0) {
-                $('#spinner').removeClass('show');
-            }
-        }, 1);
-    };
-    spinner();
-    
-    
-    // Initiate the wowjs
-    new WOW().init();
-
-
-   
-
-    
-
-    
-
-    
-    $(document).ready(function () {
-      var isScrolling = false;
-      var $navbarCollapse = $('.navbar-collapse');
-    
-      // Add or remove 'navbar-scrolled' class based on scroll position
-      function updateNavbar() {
-        var scrollDistance = $(window).scrollTop();
-        if (scrollDistance > 300) {
-          $('.navbar').addClass('navbar-scrolled');
-        } else {
-          $('.navbar').removeClass('navbar-scrolled');
-        }
+  // Spinner
+  var spinner = function () {
+    setTimeout(function () {
+      if ($("#spinner").length > 0) {
+        $("#spinner").removeClass("show");
       }
-    
-      // Assign active class to nav links while scrolling
-      function updateActiveSection() {
-        var scrollDistance = $(window).scrollTop();
-        var windowHeight = $(window).height();
-        var isBottomReached = (scrollDistance + windowHeight) >= $(document).height();
-    
-        $('.section').each(function () {
-          var sectionTop = $(this).offset().top;
-          var sectionHeight = $(this).outerHeight();
-    
-          if (
-            (scrollDistance >= sectionTop - windowHeight / 2 && scrollDistance < sectionTop + sectionHeight - windowHeight / 2) ||
-            (isBottomReached && $(this).is(':last-child'))
-          ) {
-            var sectionId = $(this).attr('id');
-            $('.navbar-nav a').removeClass('active');
-            $('.navbar-nav a[href="#' + sectionId + '"]').addClass('active');
-          }
-        });
-      }
-    
-      // Scroll event listener
-      $(window).scroll(function () {
-        if (!isScrolling) {
-          isScrolling = true;
-          updateNavbar();
-          updateActiveSection();
-          setTimeout(function () {
-            isScrolling = false;
-          }, 250);
-        }
-      });
-    
-      // Refresh scroll navigation on page load
-      updateNavbar();
-      updateActiveSection();
-    
-      // Add click event listener to nav links
-      $('.navbar-nav a').on('click', function (event) {
-        event.preventDefault();
-        var targetSection = $(this).attr('href');
-        var targetOffset = $(targetSection).offset().top;
-        var navHeight = $('.navbar').outerHeight();
-        var scrollDistance = targetOffset;
-    
-        $('html, body').animate({
-          scrollTop: scrollDistance
-        }, 100, function () {
-          updateActiveSection();
-        });
-    
-        // Close the navbar after clicking a navigation item
-        $navbarCollapse.collapse('hide');
-    
-        $('.navbar-nav a').removeClass('active');
-        $(this).addClass('active');
-      });
-    
-      // Refresh scroll navigation on window resize
-      $(window).resize(function () {
-        updateActiveSection();
-      });
-    
-      // Refresh scroll navigation on content change
-      $(window).on('load', function () {
-        updateActiveSection();
-      });
-    });
-    
-    
-      
-      
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 300) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 10, 'easeInOutExpo');
-        return false;
-    });
-    
+    }, 1);
+  };
+  spinner();
 
-    // Typed Initiate
-    if ($('.typed-text-output').length == 1) {
-        var typed_strings = $('.typed-text').text();
-        var typed = new Typed('.typed-text-output', {
-            strings: typed_strings.split(', '),
-            typeSpeed: 100,
-            backSpeed: 20,
-            smartBackspace: false,
-            loop: true
-        });
+  // Initiate the wowjs
+  new WOW().init();
+
+  $(document).ready(function () {
+    var isScrolling = false;
+    var $navbarCollapse = $(".navbar-collapse");
+
+    // Add or remove 'navbar-scrolled' class based on scroll position
+    function updateNavbar() {
+      var scrollDistance = $(window).scrollTop();
+      if (scrollDistance > 300) {
+        $(".navbar").addClass("navbar-scrolled");
+      } else {
+        $(".navbar").removeClass("navbar-scrolled");
+      }
     }
 
+    // Assign active class to nav links while scrolling
+    function updateActiveSection() {
+      var scrollDistance = $(window).scrollTop();
+      var windowHeight = $(window).height();
+      var isBottomReached =
+        scrollDistance + windowHeight >= $(document).height();
 
+      $(".section").each(function () {
+        var sectionTop = $(this).offset().top;
+        var sectionHeight = $(this).outerHeight();
 
-    
-    $(document).ready(function() {
-  $('.refresh-link').click(function(e) {
-    e.preventDefault();
+        if (
+          (scrollDistance >= sectionTop - windowHeight / 2 &&
+            scrollDistance < sectionTop + sectionHeight - windowHeight / 2) ||
+          (isBottomReached && $(this).is(":last-child"))
+        ) {
+          var sectionId = $(this).attr("id");
+          $(".navbar-nav a").removeClass("active");
+          $('.navbar-nav a[href="#' + sectionId + '"]').addClass("active");
+        }
+      });
+    }
 
-    // Restore the original color and remove any selection styles from the icon and link
-    var link = $(this);
-    var icon = link.find('i');
-    
-    // Remove any classes that modify the icon's color or hover effects
-    icon.removeClass('clicked'); // Replace 'clicked' with the class name that changes the color
-    icon.removeClass('hover-class'); // Replace 'hover-class' with any class applied on hover
-    
-    link.blur(); // Remove focus from the link
-    
-    // Allow the link to open in a new tab after a short delay (e.g., 300 milliseconds)
-    setTimeout(function() {
-      var newTab = window.open(link.attr('href'), '_blank');
-      newTab.focus();
-    }, 300);
+    // Scroll event listener
+    $(window).scroll(function () {
+      if (!isScrolling) {
+        isScrolling = true;
+        updateNavbar();
+        updateActiveSection();
+        setTimeout(function () {
+          isScrolling = false;
+        }, 250);
+      }
+    });
+
+    // Refresh scroll navigation on page load
+    updateNavbar();
+    updateActiveSection();
+
+    // Add click event listener to nav links
+    $(".navbar-nav a").on("click", function (event) {
+      event.preventDefault();
+      var targetSection = $(this).attr("href");
+      var targetOffset = $(targetSection).offset().top;
+      var navHeight = $(".navbar").outerHeight();
+      var scrollDistance = targetOffset;
+
+      $("html, body").animate(
+        {
+          scrollTop: scrollDistance,
+        },
+        100,
+        function () {
+          updateActiveSection();
+        }
+      );
+
+      // Close the navbar after clicking a navigation item
+      $navbarCollapse.collapse("hide");
+
+      $(".navbar-nav a").removeClass("active");
+      $(this).addClass("active");
+    });
+
+    // Refresh scroll navigation on window resize
+    $(window).resize(function () {
+      updateActiveSection();
+    });
+
+    // Refresh scroll navigation on content change
+    $(window).on("load", function () {
+      updateActiveSection();
+    });
   });
-});
 
+  // Back to top button
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 300) {
+      $(".back-to-top").fadeIn("slow");
+    } else {
+      $(".back-to-top").fadeOut("slow");
+    }
+  });
+  $(".back-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 10, "easeInOutExpo");
+    return false;
+  });
 
-   
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
+  // Typed Initiate
+  if ($(".typed-text-output").length == 1) {
+    var typed_strings = $(".typed-text").text();
+    var typed = new Typed(".typed-text-output", {
+      strings: typed_strings.split(", "),
+      typeSpeed: 100,
+      backSpeed: 20,
+      smartBackspace: false,
+      loop: true,
     });
+  }
 
+  $(document).ready(function () {
+    $(".refresh-link").click(function (e) {
+      e.preventDefault();
 
-    // Skills
-    $('.skill').waypoint(function () {
-        $('.progress .progress-bar').each(function () {
-            $(this).css("width", $(this).attr("aria-valuenow") + '%');
-        });
-    }, {offset: '80%'});
+      // Restore the original color and remove any selection styles from the icon and link
+      var link = $(this);
+      var icon = link.find("i");
 
+      // Remove any classes that modify the icon's color or hover effects
+      icon.removeClass("clicked"); // Replace 'clicked' with the class name that changes the color
+      icon.removeClass("hover-class"); // Replace 'hover-class' with any class applied on hover
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+      link.blur(); // Remove focus from the link
+
+      // Allow the link to open in a new tab after a short delay (e.g., 300 milliseconds)
+      setTimeout(function () {
+        var newTab = window.open(link.attr("href"), "_blank");
+        newTab.focus();
+      }, 300);
     });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
-        $(this).addClass('active');
+  });
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
-    });
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
+  });
 
-     // Portfolio isotope and filter
-     var portfolio1Isotope = $('.portfolio1-container').isotope({
-        itemSelector: '.portfolio1-item',
-        layoutMode: 'fitRows'
-    });
-    $('#portfolio1-flters li').on('click', function () {
-        $("#portfolio1-flters li").removeClass('active');
-        $(this).addClass('active');
+  // Skills
+  $(".skill").waypoint(
+    function () {
+      $(".progress .progress-bar").each(function () {
+        $(this).css("width", $(this).attr("aria-valuenow") + "%");
+      });
+    },
+    { offset: "80%" }
+  );
 
-        portfolio1Isotope.isotope({filter: $(this).data('filter')});
-    });
+  // Portfolio isotope and filter
+  var portfolioIsotope = $(".portfolio-container").isotope({
+    itemSelector: ".portfolio-item",
+    layoutMode: "fitRows",
+  });
+  $("#portfolio-flters li").on("click", function () {
+    $("#portfolio-flters li").removeClass("active");
+    $(this).addClass("active");
 
-
-    
-
-    
+    portfolioIsotope.isotope({ filter: $(this).data("filter") });
+  });
 })(jQuery);
-
